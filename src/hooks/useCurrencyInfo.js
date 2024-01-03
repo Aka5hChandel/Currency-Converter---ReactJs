afd5937b71055bf58c5aca769a97b9995a042435
Aka5hChandel <155255568+Aka5hChandel@users.noreply.github.com>
2024-01-03 18:36:56 +0530

    Update useCurrencyInfo.js

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,28 +1,4 @@
-// import { useState } from "react"
-// import { useEffect } from "react"
 
-// //  it is a custom hook 
-
-// function useCurrencyinfo(currency) {
-//     const [data, setData] = useState({})
-//     useEffect(() => {
-//         // api call
-//         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-//             //  response (res) convert into json 
-//             .then((res) => res.json())
-//             // now res come in json format
-//             .then(res) = setData(res[currency])
-//         // currency is a key value.
-//         console.log(data);
-
-//     }, [currency])
-//     console.log(data);
-
-//     return data
-
-// }
-
-// export default useCurrencyinfo;
 
 import { useEffect, useState } from "react"
 
